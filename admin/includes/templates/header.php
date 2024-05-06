661569cc306c95bda66714de0522de3a2143af8a
Eslam-Nazer <eslam.nazer43@gmail.com>
2024-05-06 22:25:44 +0300

    Header file for call up css files and included in initialize file to start pages

---
+++ admin/includes/templates/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="referrer" content="strict-origin" />
+    <title><?php getTitle() ?></title>
+    <link rel="stylesheet" href="<?php echo $cssDir; ?>bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo $cssDir; ?>fontawesome.css">
+    <link rel="stylesheet" href="<?php echo $cssDir; ?>jquery-ui.css">
+    <link rel="stylesheet" href="<?php echo $cssDir; ?>jquery.selectBoxIt.css">
+    <link rel="stylesheet" href="<?php echo $cssDir; ?>brands.css">
+    <link rel="stylesheet" href="<?php echo $cssDir; ?>solid.css">
+    <link rel="stylesheet" href="<?php echo $cssDir; ?>backend.css">
+</head>
+<body>
+
+
