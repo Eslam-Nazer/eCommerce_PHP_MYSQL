653fa2a385e7d2822db4921c7b8cf77cba961fb8
Eslam-Nazer <eslam.nazer43@gmail.com>
2024-05-16 09:02:58 +0300

    Edit and add query for fetch names of categories

--- categories.php
+++ categories.php
@@ -1,11 +1,14 @@
-<?php include 'init.php'; ?>
+<?php
+ob_start();
+session_start();
+include 'init.php'; ?>
 
 
 <div class="container">
     <h1 class="text-center">Show items</h1>
         <div class="row gx-5">
             <?php 
-            $items = getItems('Cat_ID' ,$_GET['pageid']);
+            $items = getItems('Cat_ID' ,$_GET['pageid'], 1);
             foreach ($items as $item) {
                 echo '<div class="col-sm-6 col-md-4 col-lg-3">';
                 echo '<div class="card item-box" style="width: 18rem;">';
@@ -14,6 +17,7 @@
                 echo '<div class="card-body">';
                 echo '<h3 class="card-title">' . $item['Name'] . '</h3>';
                 echo '<p class="card-text">' . $item['Description'] . '</p>';
+                echo '<div class="date">' . $item['Add_Date'] . '</div>';
                 echo '<a href="items.php?itemid=' . $item['Item_ID'] . '" class="btn btn-primary">Show Item</a>';
                 echo '</div>';
                 echo '</div>';
@@ -24,4 +28,7 @@
 </div>
 
 
-<?php include $tplDir . 'footer.php'; ?>
+<?php 
+include $tplDir . 'footer.php'; 
+ob_end_flush();
+?>
