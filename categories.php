97e86ddd32cd06213b993bff430af8922904dc51
Eslam-Nazer <eslam.nazer43@gmail.com>
2024-05-26 10:39:23 +0300

    Add margin top to error message

--- categories.php
+++ categories.php
@@ -2,18 +2,21 @@
 ob_start();
 session_start();
 include 'init.php'; 
-
-$stmt = $con->prepare('SELECT Name FROM categories WHERE ID = '. $_GET['pageid']);
-$stmt->execute();
-$categoryName = $stmt->fetch();
 ?>
 
-
 <div class="container">
-    <h1 class="text-center"><?php echo $categoryName['Name'] ?></h1>
+    <?php
+    if(isset($_GET['pageid']) && is_numeric($_GET['pageid'])) {
+        $pageId = intval($_GET['pageid']);
+        $stmt = $con->prepare('SELECT Name FROM categories WHERE ID = '. $pageId);
+        $stmt->execute();
+        $categoryName = $stmt->fetch(); 
+        ?>
+
+        <h1 class="text-center"><?php echo $categoryName['Name'] ?></h1>
         <div class="row gx-5">
             <?php 
-            $items = getItems('Cat_ID' ,$_GET['pageid'], 1);
+            $items = getItems('Cat_ID' ,$pageId, 1);
             foreach ($items as $item) {
                 echo '<div class="col-sm-6 col-md-4 col-lg-3">';
                 echo '<div class="card item-box" style="width: 18rem;">';
@@ -30,6 +33,10 @@ $categoryName = $stmt->fetch();
             }
             ?>
         </div>
+    <?php } else {
+        $errorMsg = '<div class="alert alert-danger mt-4" role="alert"><i class="fa-solid fa-circle-xmark"></i> You Don\'t Specify Page ID </div>';
+        redirectHome($errorMsg);
+    } ?>
 </div>
 
 
