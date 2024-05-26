16199ac21fb254ed914ed822ce775e6b04ecdf25
Eslam-Nazer <eslam.nazer43@gmail.com>
2024-05-26 10:40:48 +0300

    To view items have same tags only Approve tags

---
+++ tags.php
@@ -0,0 +1,47 @@
+<?php
+ob_start();
+session_start();
+include 'init.php'; 
+?>
+
+<div class="container">
+    <?php
+    if(isset($_GET['name']) && $_GET['name'] != null ) {
+        $tag = $_GET['name'];
+        echo '<h1 class="text-center">' . $tag . '</h1>';
+        ?>
+        <div class="row gx-5">
+            <?php 
+            $stmt = $con->prepare("SELECT * FROM items WHERE tags LIKE '%{$tag}%'");
+            $stmt->execute();
+            $items = $stmt->fetchAll();
+            // echo '<pre>';
+            // print_r($items);
+            // echo '</pre>';
+            foreach ($items as $item) {
+                echo '<div class="col-sm-6 col-md-4 col-lg-3">';
+                echo '<div class="card item-box" style="width: 18rem;">';
+                echo '<span class="price-tag">' . $item['Price'] . '</span>';
+                echo '<img src="img.jpg" class="card-img-top img-thumbnail" alt="...">';
+                echo '<div class="card-body">';
+                echo '<h3 class="card-title">' . $item['Name'] . '</h3>';
+                echo '<p class="card-text">' . $item['Description'] . '</p>';
+                echo '<div class="date">' . $item['Add_Date'] . '</div>';
+                echo '<a href="items.php?itemid=' . $item['Item_ID'] . '" class="btn btn-primary">Show Item</a>';
+                echo '</div>';
+                echo '</div>';
+                echo '</div>';
+            }
+            ?>
+        </div>
+    <?php } else {
+        $errorMsg = '<div class="alert alert-danger mt-4" role="alert"><i class="fa-solid fa-circle-xmark"></i> You Must choose tag name</div>';
+        redirectHome($errorMsg);
+    } ?>
+</div>
+
+
+<?php 
+include $tplDir . 'footer.php'; 
+ob_end_flush();
+?>
